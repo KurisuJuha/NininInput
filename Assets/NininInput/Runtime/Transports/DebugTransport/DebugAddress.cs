6ea18e8fabc6afc3734555455f4516e679698f63
KYapp <kukiyomenaisapp@gmail.com>
2023-02-04 17:52:25 +0900

    DebugTransport用のaddress

---
+++ Assets/NininInput/Runtime/Transports/DebugTransport/DebugAddress.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace JuhaKurisu.PopoTools.Ninin.Tranports.DebugTranport
+{
+    public struct DebugAddress
+    {
+        public readonly string address;
+
+        public DebugAddress(string address)
+        {
+            this.address = address;
+        }
+    }
+}
